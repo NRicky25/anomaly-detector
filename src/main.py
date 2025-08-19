b14fbd202108898ddd296ca16932704b82a8a1fe
N3-Ricky <165741930+NRicky25@users.noreply.github.com>
2025-08-19 18:16:10 +1000

    Fix: Middleware

--- src/main.py
+++ src/main.py
@@ -127,7 +127,8 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-origins = ["http://localhost", "http://localhost:5173", "http://127.0.0.1:5173", "https://elegant-crostata-101fff.netlify.app"]
+origins = ["http://localhost", "http://localhost:5173", "http://127.0.0.1:5173", "https://anomaly-detector-api.netlify.app"]
+
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 class TransactionInput(BaseModel):
     Time: float = Field(...)
